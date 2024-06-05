86c5f167fd879dde7ad3a7d352a09a3580891c6d
JackyYangPassion <yang.jiaqi.hadoop@gmail.com>
2024-06-05 23:19:12 +0800

    add muti thread schedule

--- src/main/java/client/GrpcClientExample.java
+++ src/main/java/client/GrpcClientExample.java
@@ -1,37 +1,74 @@
 package client;
 
-import io.grpc.StatusRuntimeException;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.*;
 
+
+/**
+ * Java 多线程模型
+ * 一个线程池：   执行 RPC 调用
+ * 一个守护线程： 执行记时，超时后调用 Cancel
+ */
 public class GrpcClientExample {
 
 
-    public static void main(String[] args) throws InterruptedException {
-        // 假设我们有一个Greeter服务和相应的proto文件生成的代码
 
+    public static void main(String[] args) {
 
-        // 创建一个线程来执行gRPC调用
-        Thread thread = new Thread(() -> {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+        final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(10);
+        ExecutorService executorService = new ThreadPoolExecutor(10, 10,
+                0L, TimeUnit.MILLISECONDS, queue,
+                new ThreadPoolExecutor.AbortPolicy());
+
+        while (true){
+            final CompletableFuture<Object> evaluationFuture = new CompletableFuture<>();
+            final FutureTask<Void> evalFuture = new FutureTask<>(() -> {
+                try {
+                    ClientDemo clientDemo = new ClientDemo();
+                    clientDemo.remoteCall("【JackyGraph】");
+
+                    evaluationFuture.complete("success");
+                } catch (Throwable ex) {
+
+                }
+
+                return null;
+            });
+
+            long scriptEvalTimeOut = 1000;
+            final WeakReference<CompletableFuture<Object>> evaluationFutureRef = new WeakReference<>(evaluationFuture);
+            final Future<?> executionFuture =  executorService.submit(evalFuture);
+
+            // Schedule a timeout in the thread pool for future execution
+            final ScheduledFuture<?> sf = scheduledExecutorService.schedule(() -> {
+                if (executionFuture.cancel(true)) {
+                    final CompletableFuture<Object> ef = evaluationFutureRef.get();
+                    if (ef != null) {
+                        ef.completeExceptionally(new TimeoutException(
+                                String.format("Evaluation exceeded the configured 'evaluationTimeout' threshold of %s ms or evaluation was otherwise cancelled directly for request [%s]", scriptEvalTimeOut, "time")));
+                    }
+                }
+            }, scriptEvalTimeOut, TimeUnit.MILLISECONDS);
+
+            // Cancel the scheduled timeout if the eval future is complete or the script evaluation failed with exception
+            evaluationFuture.handleAsync((v, t) -> {
+                if (!sf.isDone()) {
+                    System.out.println("Killing scheduled timeout on script evaluation - {} - as the eval completed (possibly with exception)."+"script");
+                    sf.cancel(true);
+                }
+
+                // no return is necessary - nothing downstream is concerned with what happens in here
+                return null;
+            }, scheduledExecutorService);
+
             try {
-                // 执行gRPC调用
-                ClientDemo clientDemo = new ClientDemo();
-                clientDemo.remoteCall("【JackyGraph】");
-            } catch (StatusRuntimeException e) {
-                // 捕获并打印异常
-                System.err.println("RPC failed: " + e.getStatus());
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
-
-        // 启动线程
-        thread.start();
-
-        try {
-            Thread.sleep(100); // 等待一段时间，让gRPC调用开始
-            thread.interrupt(); // 中断线程，期望触发CANCELLED异常
-        } catch (InterruptedException ie) {
-            ie.printStackTrace();
         }
-
-
     }
 
 }
